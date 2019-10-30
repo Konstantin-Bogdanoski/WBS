dc42b39594b79be828b805dbc9897eec6edb5355
Konstantin Bogdanoski <konstantin.b@live.com>
2019-10-30 17:57:10 +0100

    - Update Jena dependencies

---
+++ Homeworks/src/main/java/Homework_01.java
@@ -0,0 +1,12 @@
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+/**
+ * @author Konstantin Bogdanoski (konstantin.b@live.com)
+ */
+
+public class Homework_01 {
+    public static void main(String[] args) {
+        Model model = ModelFactory.createDefaultModel();
+    }
+}
