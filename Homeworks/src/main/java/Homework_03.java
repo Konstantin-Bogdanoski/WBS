99098f0a868ec25725f0453d1c1e3c83e411af1a
Konstantin Bogdanoski <konstantin.b@live.com>
2019-11-17 20:53:06 +0100

    - Homework02

--- Homeworks/src/main/java/Homework_03.java
+++ Homeworks/src/main/java/Homework_03.java
@@ -3,6 +3,6 @@
  */
 public class Homework_03 {
     public static void main(String[] args) {
-
+        //Work with PROTEGE v5, the final ontology is saved in H-03_OWL Directory (.../main/java/H-03_OWL/*)
     }
 }
